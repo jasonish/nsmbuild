80c38a01e2a7abe17f472850fdb50132f6559b4e
Jason Ish <ish@unx.ca>
2014-04-07 10:19:00 -0600

    Add Bro 2.2.

---
+++ builds/bro/2.2/build.py
@@ -0,0 +1,15 @@
+name = "bro"
+version = "2.2"
+rev = 1
+
+source = "http://www.bro.org/downloads/release/%(name)s-%(version)s.tar.gz"
+
+def init(build):
+    pass
+
+def configure(build):
+    build.call("./configure --prefix=#{prefix}")
+
+def build(build):
+    build.call("make")
+    build.call("make install DESTDIR=#{fakeroot}")
